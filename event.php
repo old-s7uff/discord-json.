27beb2070d3f78d4e8966a0260e4b89e6fe9f435
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-10-17 05:15:47 +0000

    Update event.php

--- event.php
+++ event.php
@@ -1,6 +1,36 @@
 <?php
-function dope_PushEvents($event)
+function dope_PushEvents($flavor, $image, $serverID)
 {
+    $event = array(
+      "username" => "DOPE-BOT",
+      "embeds" => array([
+          "title" => "**CREATED!**",
+          "description" => "*NEW CLOUD SRV CREATED!*",
+          "fields" => array(
+              array(
+                  "name" => "**FLAVOR**",
+                  "value" => "**GRA3-TT**",
+                  "inline" => true
+              ),
+              array(
+                  "name" => "**IMAGE**",
+                  "value" => "**GRA3-IMG**",
+                  "inline" => true
+              ),
+              array(
+                  "name" => "**SERVER ID**",
+                  "value" => "**65-4-564-65-4654654**",
+                  "inline" => true
+              ),
+              array(
+                  "name" => "**DATE/TIME**",
+                  "value" => "**123/123/123**",
+                  "inline" => true
+              ),
+          ),
+          "color" => 65280,
+      ])
+    );
     $curl = curl_init("https://discordapp.com/api/webhooks/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($event));
@@ -8,35 +38,6 @@ function dope_PushEvents($event)
     return curl_exec($curl);
 }
 
-$event = array(
-  "username" => "DOPE-BOT",
-  "embeds" => array([
-      "title" => "**CREATED!**",
-      "description" => "*NEW CLOUD SRV CREATED!*",
-      "fields" => array(
-          array(
-              "name" => "**FLAVOR**",
-              "value" => "**GRA3-TT**",
-              "inline" => true
-          ),
-          array(
-              "name" => "**IMAGE**",
-              "value" => "**GRA3-IMG**",
-              "inline" => true
-          ),
-          array(
-              "name" => "**SERVER ID**",
-              "value" => "**65-4-564-65-4654654**",
-              "inline" => true
-          ),
-          array(
-              "name" => "**DATE/TIME**",
-              "value" => "**123/123/123**",
-              "inline" => true
-          ),
-      ),
-      "color" => 65280,
-  ])
-);
-dope_PushEvents($event);
+
+dope_PushEvents($flavor, $image, $serverID);
 ?>
