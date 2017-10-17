a4b014c845777fdeb4cabbcf7e179e6152992888
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-10-17 05:13:07 +0000

    Update fk.php

--- fk.php
+++ fk.php
@@ -1,7 +1,7 @@
 <?php
-function PushEvents($event)
+function dope_PushEvents($event)
 {
-    $curl = curl_init("https://discordapp.com/api/webhooks/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    $curl = curl_init("https://discordapp.com/api/webhooks/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($event));
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -10,7 +10,33 @@ function PushEvents($event)
 
 $event = array(
   "username" => "DOPE-BOT",
-  "content" => "TEST!"
+  "embeds" => array([
+      "title" => "**CREATED!**",
+      "description" => "*NEW CLOUD SRV CREATED!*",
+      "fields" => array(
+          array(
+              "name" => "**FLAVOR**",
+              "value" => "**GRA3-TT**",
+              "inline" => true
+          ),
+          array(
+              "name" => "**IMAGE**",
+              "value" => "**GRA3-IMG**",
+              "inline" => true
+          ),
+          array(
+              "name" => "**SERVER ID**",
+              "value" => "**65-4-564-65-4654654**",
+              "inline" => true
+          ),
+          array(
+              "name" => "**DATE/TIME**",
+              "value" => "**123/123/123**",
+              "inline" => true
+          ),
+      ),
+      "color" => 65280,
+  ])
 );
-PushEvents($event);
+dope_PushEvents($event);
 ?>
